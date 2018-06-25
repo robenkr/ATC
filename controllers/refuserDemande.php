5f6934dc9c00e62c00da19114973a4cb344edc84
Roland Beni <roland.beni1@gmail.com>
2018-06-25 12:40:19 +0200

    creation du controller(refuserDemande.php)=>Maureen

---
+++ controllers/refuserDemande.php
@@ -0,0 +1,20 @@
+<?php
+    require_once ('../models/dao/connexiondb.class.php');
+    require_once ('../models/structure/Formation.class.php');
+    require_once ('../models/dao/formationsDAO.php');
+
+
+    if (isset($_GET['idCours'],$_GET['idEntreprise'])){
+        $idCours = $_GET['idCours'];
+        $idEntreprise = $_GET['idEntreprise'];
+        $formation = new Formation(0,0,0,$idEntreprise,$idAdmin,$idCours,0);
+        $formationdao = new FormationsDAO();
+
+        $res = $formationdao->refuser($formation);
+
+        header('Location: ../admin/index.php');
+    }
+    else{
+        header('Location: ../admin/index.php');
+    }
+?>
